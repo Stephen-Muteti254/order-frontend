eb8fb86ea530975ce4dd7cd210e380e85c085bff
Stephen Muteti <stephenmuteti09@gmail.com>
2026-05-31 09:31:45 +0300

    Initial commit

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || 'https://order-bkd.onrender.com/api/v1';
+  import.meta.env.VITE_API_URL || 'https://order-bkd-c8im.onrender.com/api/v1';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
